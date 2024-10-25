09e7b9aa9f06fe00caaae58117e2852c622a6869
PeyHunter <Nando.goat@gmail.com>
2024-10-25 12:49:51 +0200

    Added Order Attributes

--- src/Ordre.java
+++ src/Ordre.java
@@ -1,4 +1,24 @@
 public class Ordre {
 
     public Ordre() {}
+
+    private int ordreNr;
+    private int bestillingsTid;
+    private int bestillingsDato;
+    private Pizza pizzaObject;
+
+    public int getOrdreNr(){
+        return ordreNr;
+    }
+
+    public int getBestillingsbestillingsTid() {
+        return bestillingsDato;
+    }
+    public int bestillingsDato(){
+        return bestillingsDato;
+    }
+
+    public Pizza getPizzaObject() {
+        return pizzaObject;
+    }
 }
